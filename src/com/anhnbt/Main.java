ceb24b898584ad613eed455c5a417f8c730495dc
ANHNBT-DEV <anhnbt.dev@outlook.com>
2020-11-01 16:28:44 +0700

    feat: add delete confirm

--- src/com/anhnbt/Main.java
+++ src/com/anhnbt/Main.java
@@ -3,12 +3,14 @@ package com.anhnbt;
 import com.anhnbt.common.Menu;
 import com.anhnbt.manager.StudentManager;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static final int exitProgram = 0;
     public static final int addStudent = 1;
     public static final int editStudent = 2;
+    public static final int deleteStudent = 3;
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -26,6 +28,7 @@ public class Main {
                 menu.showHeader();
 
                 choice = scanner.nextInt();
+                scanner.nextLine();
                 switch (choice) {
                     case exitProgram:
                         menu.quit();
@@ -36,12 +39,21 @@ public class Main {
                     case editStudent:
                         System.out.println("Your choice One");
                         break;
+                    case deleteStudent:
+                        System.out.print("Are you sure you want to delete? [Y/n]: ");
+                        char confirm = scanner.next().charAt(0);
+                        if (confirm == 'Y') {
+                            System.out.println("Done");
+                        } else {
+                            continue;
+                        }
+                        break;
                     default:
                         System.out.println("No choice!");
                 }
             } while (choice != 0);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (InputMismatchException ex) {
+            System.out.println("Du lieu nhap vao khong dung!");
         }
     }
 }
